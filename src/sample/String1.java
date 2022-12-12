18b094348646afb35759fcd17915e0f99bac6b2e
Dell <Dell@DESKTOP-HH8MK94>
2022-12-12 22:06:32 +0530

    [Akshay]:String1

---
+++ src/sample/String1.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class String1 
+{
+public static void main(String[] args) 
+{
+    String s1 = new String("Velocity");	
+    String s2 = new String("Velocity");
+    
+    //I want result as true and also false do it by using string methods
+}
+}
