2449c731b60407dc4461fe99b67dc14b1bc2987e
SACHIN <SACHIN@DESKTOP-GICICBH>
2022-12-12 23:04:12 +0530

    [SACHIN]:String1

--- src/sample/String1.java
+++ src/sample/String1.java
@@ -8,5 +8,14 @@ public static void main(String[] args)
     String s2 = new String("Velocity");
     
     //I want result as true and also false do it by using string methods
+    
+    
+    
+    System.out.println(s1.equals(s2));   // true
+    
+    System.out.println(s1==s2);     // false  -> because object created using new keyword.
+    
+    
+    
 }
 }
